61c8187555fd4e1993634e2f05661badfc8c08df
Daniel Kuang <danielkuang7@github.com>
2019-10-28 20:19:52 -0400

    Added header

--- gotnews-app/src/components/Header.js
+++ gotnews-app/src/components/Header.js
@@ -4,6 +4,7 @@ class Header extends Component{
     render(){
         return(
             <div className="header"> 
+                <div className="titles"><h1>News Report</h1></div>
             </div>
         );
     };
