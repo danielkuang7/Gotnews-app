3265c66a1360e9338b50eb6ffe8d3fdf71c72f16
Daniel Kuang <danielkuang7@github.com>
2019-10-31 20:26:04 -0400

    Added state

--- gotnews-app/src/components/Search.js
+++ gotnews-app/src/components/Search.js
@@ -3,6 +3,9 @@ import React, { Component } from 'react';
 class Search extends Component {
     constructor(props){
         super(props)
+        this.state={
+            value: ''
+        }
     }
 
     render(){
