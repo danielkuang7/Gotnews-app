b841342b73de624b23aa52d1ac7a550c525e3d3e
Daniel Kuang <danielkuang7@github.com>
2019-11-22 20:09:55 -0500

    Added contents to app.js

--- gotnews-app/src/App.js
+++ gotnews-app/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Search from './components/Search';
+import Contents from './components/Contents';
 
 function App() {
   return (
     <div className="App">
     <Header />
     <Search submit={this.handleSubmit} />
+    <Contents news={this.state.news}/>
     </div>
   );
 }
